e48fe25f28b613bc682db8c5413a71d60973550c
Sudheer Konduboina <konduboinasudheer@gmail.com>
2026-02-22 16:01:46 +0530

    updated the code for pages

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const repo = "Round1_praavalegal_task";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",              // static export (creates /out)
-  trailingSlash: true,           // helps with GitHub Pages routing
+  output: "export",              // <-- creates /out with index.html
+  trailingSlash: true,
   basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
-  images: { unoptimized: true }, // required for GitHub Pages
+  images: { unoptimized: true }, // required for static hosting
 };
 
 export default nextConfig;
